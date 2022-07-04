5656c5f04602dca84ea963fb2d918bbbec510170
Lebogang Mogane <mogane420@gmail.com>
2022-07-04 11:34:58 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="title">Hello React!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
